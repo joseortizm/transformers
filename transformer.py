6d592e4485bbf7469cd3333580d81e006d5afaee
Jose <pepeisa.ortiz@gmail.com>
2024-08-24 22:44:56 -0500

    add Positional Embedding class

--- transformer.py
+++ transformer.py
@@ -14,11 +14,34 @@ else:
 MAX_SEQ_LEN = 30 #128
 
 class PositionalEmbedding(nn.Module):
+    """
+    pos_embed_matrix: rows is max sequence (in sentence), col is d_model (numbre of elements in embedding)
+                      default to all zeros
+
+    token_pos: token position
+    div_term:  10000^(2i/d_model)
+    pos_embed_matrix[:, 0::2]: even
+    pos_embed_matrix[:, 1::2]: odd
+    pos_embed_matrix:
+
+
+    """
+
     def __init__(self, d_model, max_seq_len = MAX_SEQ_LEN):
-        pass
+        super().__init__()
+        self.pos_embed_matrix = torch.zeros(max_seq_len, d_model, device=device)
+        token_pos = torch.arange(0, max_seq_len, dtype = torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0)/d_model))
+        self.pos_embed_matrix[:, 0::2] = torch.sin(token_pos * div_term)
+        self.pos_embed_matrix[:, 1::2] = torch.cos(token_pos * div_term)
+        self.pos_embed_matrix = self.pos_embed_matrix.unsqueeze(0).transpose(0,1)
 
     def forward(self, x):
-        pass
+        """
+        add "positional encodings" to the input embeddings at the bottoms of the encoder and decoder stacks
+        """
+        return x + self.pos_embed_matrix[:x.size(0), :]
+        
 
 
 class Encoder(nn.Module):
@@ -67,6 +90,9 @@ class Transformer(nn.Module):
     dim_feedforward: the dimension of the feedforward network model (default=2048).
     d_ff: ... and the inner-layer has dimensionality dff = 2048.
 
+    -
+    encoder_embedding is Input Embedding / decoder_embedding is Output Enbedding
+    
     num_encoder_layers: the number of sub-encoder-layers in the encoder (default=6).
     num_decoder_layers: the number of sub-decoder-layers in the decoder (default=6).
     num_layers: The encoder is composed of a stack of N = 6 identical layers. 
@@ -95,7 +121,28 @@ class Transformer(nn.Module):
 
     
     def forward(self, source, target):
-        pass
+        """
+        source: 1)Input Embedding, 2)Positional Encoding
+        1)  In the embedding layers, we multiply those weights by square root of d_model
+        self.encoder_embedding(source) * math.sqrt(self.encoder_embedding.embedding_dim)
+        where self.encoder_embedding.embedding_dim is d_model doc: https://pytorch.org/docs/stable/generated/torch.nn.Embedding.html#embedding
+        2) self.pos_embedding(source) 
+
+        target: 1)Output Embedding, 2)Positional Encoding
+
+        """
+        source_mask, target_mask = self.mask(source, target)
+
+        source = self.encoder_embedding(source) * math.sqrt(self.encoder_embedding.embedding_dim)
+        source = self.pos_embedding(source)
+        encoder_output = self.encoder(source, source_mask) #Create Encoder and get output 
+
+        target = self.decoder_embedding(target) * math.sqrt(self.decoder_embedding.embedding_dim)
+        target = self.pos_embedding(target)
+        output = self.decoder(target, encoder_output, target_mask, source_mask) #Create Decoder. source_mask is encoder_mask 
+
+        return output_layer(output)
+
 
     def mask(self, source, target):
         """
@@ -103,4 +150,11 @@ class Transformer(nn.Module):
         
         """
         source_mask = (source != 0).unsqueeze(1).unsqueeze(2)
+        target_mask = (target != 0).unsqueeze(1).unsqueeze(2)
+        size = target.size(1) #represents the tokens in the sequence
+        no_mask = torch.tril(torch.ones((1, size, size), device=device)).bool() #elements on and below the main diagonal are True, and elements above are False.
+        target_mask = target_mask & no_mask #True only at positions where both (target_mask and no_mask) are True.
+        return source_mask, target_mask
+
+
 
