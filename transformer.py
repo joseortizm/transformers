75f5921fe85f3a2a9bee55e20f753434cc9b6ce9
Jose <pepeisa.ortiz@gmail.com>
2024-08-30 23:00:40 -0500

    add file test and translate

--- transformer.py
+++ transformer.py
@@ -1,5 +1,7 @@
 import torch
 import torch.nn as nn
+import math
+import torch.nn.functional as F
 
 torch.manual_seed(1337)
 
@@ -11,7 +13,7 @@ else:
     device = torch.device("cpu")
 
 
-MAX_SEQ_LEN = 30 #128
+#MAX_SEQ_LEN = 30 #128
 
 class PositionalEmbedding(nn.Module):
     """
@@ -212,7 +214,7 @@ class DecoderSubLayer(nn.Module):
     attention_score: first sub-layer
     cross_attn (Cross-Attention): second sub-layer
     """
-   def __init__(self, d_model, num_heads, d_ff, num_layers, dropout=0.1):
+    def __init__(self, d_model, num_heads, d_ff, num_layers, dropout=0.1):
         super().__init__()
         self.self_attn = MultiHeadAttention(d_model, num_heads)
         self.cross_attn = MultiHeadAttention(d_model, num_heads)
@@ -244,7 +246,7 @@ class Decoder(nn.Module):
     layers: Decoder layers (Nx = 6)
     norm: Layer Normalization (optional: If you want to further stabilize the final output of the Decoder, you can keep the final normalization)
     """
-     def __init__(self, d_model, num_heads, d_ff, num_layers, dropout=0.1):
+    def __init__(self, d_model, num_heads, d_ff, num_layers, dropout=0.1):
         super().__init__()
         self.layers = nn.ModuleList([DecoderSubLayer(d_model, num_heads, d_ff, dropout) for _ in range(num_layers)])
         self.norm = nn.LayerNorm(d_model)
@@ -336,7 +338,7 @@ class Transformer(nn.Module):
         target = self.pos_embedding(target)
         output = self.decoder(target, encoder_output, target_mask, source_mask) #Create Decoder. source_mask is encoder_mask 
 
-        return output_layer(output)
+        return self.output_layer(output)
 
 
     def mask(self, source, target):
@@ -352,4 +354,32 @@ class Transformer(nn.Module):
         return source_mask, target_mask
 
 
+"""
+#check Transformers():
 
+seq_len_source = 10
+seq_len_target = 10
+batch_size = 2
+input_vocab_size = 50
+target_vocab_size = 50
+
+source = torch.randint(1, input_vocab_size, (batch_size, seq_len_source))
+target = torch.randint(1, target_vocab_size, (batch_size, seq_len_target))
+
+d_model = 512
+num_heads = 8
+d_ff = 2048
+num_layers = 6
+
+model = Transformer(d_model, num_heads, d_ff, num_layers, input_vocab_size, target_vocab_size, max_len=MAX_SEQ_LEN, dropout=0.1)
+
+model = model.to(device)
+source = source.to(device)
+target = target.to(device)
+
+output = model(source, target)
+
+# Expected output shape -> [batch, seq_len_target, target_vocab_size] i.e. [2, 10, 50]
+print(f'ouput.shape {output.shape}')
+#ouput.shape torch.Size([2, 10, 50])
+"""
